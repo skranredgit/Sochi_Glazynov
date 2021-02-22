c0109c6581160e459b74c4a34694c49e480bbda0
skranredgit <75494744+skranredgit@users.noreply.github.com>
2021-02-22 18:41:45 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,44 @@
+from PyQt5.QtWidgets import QWidget, QApplication, QMainWindow, QLabel, QGridLayout
+from PyQt5.QtGui import QPainter, QPixmap, QPen, QColor
+from PyQt5.QtCore import Qt
+from PyQt5 import uic
+from random import randint
+
+
+class Test(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('untitled.ui', self)
+
+        self.pushButton.setFixedSize(100, 100)
+        self.pushButton.clicked.connect(self.circle)
+
+        self.label = QLabel()
+        canvas = QPixmap(600, 600)
+        self.label.setPixmap(canvas)
+
+        layout = QGridLayout(self.centralwidget)
+        layout.addWidget(self.pushButton, 0, 0, alignment=Qt.AlignCenter)
+        layout.addWidget(self.label, 1, 0)
+
+    def circle(self):
+        tmp = randint(10, 100)
+        x, y = [randint(10, 500) for _ in range(2)]
+        w, h = [tmp for _ in range(2)]
+        # создаем экземпляр QPainter, передавая холст (self.label.pixmap())
+        painter = QPainter(self.label.pixmap())
+        pen = QPen()
+        pen.setWidth(3)
+        pen.setColor(QColor(247, 242, 6))
+        painter.setPen(pen)
+        painter.drawEllipse(x, y, w, h)
+        painter.end()
+        self.update()
+
+
+if __name__ == '__main__':
+    import sys
+    app = QApplication(sys.argv)
+    w = Test()
+    w.show()
+    sys.exit(app.exec_())
